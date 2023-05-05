aab6d4b5f15f8213adf9fe5d8da3b2f80aab9647
Chandrika Sharma <111525029+chandrika1645@users.noreply.github.com>
2023-05-05 16:13:24 +0530

    Update todoList.js

--- To Do List/todoList.js
+++ To Do List/todoList.js
@@ -6,23 +6,19 @@ renderTodoList();
 
 function renderTodoList(){
     let todoListHTML = '';
-    
-    for(let i=0;i<todoList.length;i++){
-        const todoObject = todoList[i];
-        // const name = todoObject.name;
-        // const dueDate = todoObject.dueDate;
+    todoList.forEach(function(todoObject,index){
         const {name,dueDate} = todoObject;
 
         const html = `<div>${name}</div>
                       <div>${dueDate}</div>   
-                      <button class="delete-button" onclick=" todoList.splice(${i},1);
+                      <button class="delete-button" onclick=" todoList.splice(${index},1);
                         renderTodoList();
                        ">
                         Delete
                       </button>
                       `;
         todoListHTML += html;
-    }
+    });  
     
     document.querySelector('.js-todoList').innerHTML = todoListHTML;
 }
